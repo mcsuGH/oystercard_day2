2930864b0869a104f35194237727244d6bd7d8ef
Michael Su <mailmsuu@gmail.com>
2022-03-29 20:57:32 +0100

    removed some legacy code

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,13 +2,12 @@ require_relative 'journeylog'
 require_relative 'journey'
 
 class Oystercard
-  attr_reader :balance, :journey, :journeylog
+  attr_reader :balance, :journeylog
   LIMIT = 90
   MIN_CHARGE = 1
 
   def initialize
     @balance = 0
-    @journey = nil
     @journeylog = JourneyLog.new(journey_class: Journey.new)
   end
 
@@ -19,18 +18,16 @@ class Oystercard
 
   def touch_in(station)
     fail "Insufficient balance" if @balance < MIN_CHARGE
-    @journey = Journey.new(station)
     @journeylog.start(station)
   end
 
   def touch_out(station)
     @journeylog.destination(station)
-    @journey = nil
     deduct(MIN_CHARGE)
   end
   
   def in_journey?
-    @journey.nil? ? false : true
+    @journeylog.current_journey[:entry_station].nil? ? false : true
   end
 end
 
