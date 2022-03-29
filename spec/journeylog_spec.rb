0a3d125da692b9c16fa249240c4065791bc8d635
Michael Su <mailmsuu@gmail.com>
2022-03-29 16:59:14 +0100

    journeylog_spec updated using walkthrough

--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -1,10 +1,24 @@
 require 'journeylog'
 
 describe JourneyLog do
-  it 'starts a journey' do
+  let(:journey) { double :journey, new: station } 
+  let(:station) { double :station }
+  let(:journey_class) { double :journey_class, new: journey }
+  let(:subject) { JourneyLog.new(journey_class) }
+
+  describe '#start' do
+    it 'starts a journey' do
+      expect(journey_class).to receive(:new).with(entry_station: station)
+      subject.start(station)
+    end
+
+    it 'records a journey' do
+      allow(journey_class).to receive(:new).and_return journey
+      subject.start(station)
+      expect(subject.journeys).to include(journey)
+    end
   end
 end
-
 
 
 
